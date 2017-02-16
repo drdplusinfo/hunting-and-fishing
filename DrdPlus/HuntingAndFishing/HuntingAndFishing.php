4ec1b5d4061e4b75a073510d2f449eb31a8793e8
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-16 11:17:03 +0100

    Link to PPH rules

--- DrdPlus/HuntingAndFishing/HuntingAndFishing.php
+++ DrdPlus/HuntingAndFishing/HuntingAndFishing.php
@@ -3,6 +3,9 @@ namespace DrdPlus\HuntingAndFishing;
 
 use Granam\Strict\Object\StrictObject;
 
+/**
+ * See PPH page 132, @link https://pph.drdplus.jaroslavtyc.com/#lov_a_rybolov
+ */
 class HuntingAndFishing extends StrictObject
 {
 
